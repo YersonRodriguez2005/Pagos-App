523638d2599d74255565fe82c3ba54edd1a2d6bc
Yerson Rodriguez <rodriguezyerson2005@gmail.com>
2025-06-29 22:06:46 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import { FaMoneyBillWave, FaList, FaBars, FaTimes } from 'react-icons/fa';
 import Create from './components/create';
-import List from './components/List';
+import List from './components/list';
 import Edit from './components/edit';
 
 export default function App() {
